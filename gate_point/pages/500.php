aec669f764d910249e7e9702b6da870cd808b3fe
giuliopanda <giuliopanda@gmail.com>
2019-10-07 07:37:39 +0200

    Update 500.php
    fixed bug. Se l'errore php avviene prima di aver impostato il tempate ritornava  pagina bianca

--- gate_point/pages/500.php
+++ gate_point/pages/500.php
@@ -1,4 +1,8 @@
 <?php 
 $cData->set('content', "<h1>500 C'è stato un errore nella pagina</h1>". Gp::load()->module('logsystem', 'getCurrentPage', array('logType'=>'system') ));
 // Stampo il template
-Gp::load()->require('theme', 'index.php', $cData, false);
+if (Gp::load()->getPath('theme', 'index.php')) {
+    Gp::load()->require('theme', 'index.php', $cData, false);
+} else {
+    print $cData->get('content');
+}
